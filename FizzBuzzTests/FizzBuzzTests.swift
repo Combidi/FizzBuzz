4190a969bb928bda35e4ad779ed1e54d89c54408
Peter Combee <petercombee@gmail.com>
2023-08-10 16:59:42 +0200

    `evaluate` delivers `Buzz` on values divisible by 5

--- FizzBuzzTests/FizzBuzzTests.swift
+++ FizzBuzzTests/FizzBuzzTests.swift
@@ -10,20 +10,34 @@ import XCTest
 // Delivers fissbuzz on any number that is divisible by both 3 and 5
 
 enum FizzBuzz {
-    static func evaluate(number: Int) -> String { "Fizz" }
+    static func evaluate(number: Int) -> String {
+        return number.isMultiple(of: 5) ? "Buzz" : "Fizz"
+    }
 }
 
 final class FizzBuzzTests: XCTestCase {
 
     func test_evaluate_deliversFizzOnNumbersDivisibleByThree() {
 
-        let samples = [3, 6, 9, 12, 300]
+        let samples = [3, 6, 9, 12, 18]
         
         for sample in samples {
             
             let result = FizzBuzz.evaluate(number: sample)
             
             XCTAssertEqual(result, "Fizz", "for sample: \(sample)")
+        }
+    }
+    
+    func test_evaluate_deliversFizzOnNumbersDivisibleByFive() {
+
+        let samples = [5, 10, 15, 100, 75]
+        
+        for sample in samples {
+            
+            let result = FizzBuzz.evaluate(number: sample)
+            
+            XCTAssertEqual(result, "Buzz", "for sample: \(sample)")
         }
     }
 }
