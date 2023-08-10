288469aa5fa649c73ca6e443c000be005576b1fe
Peter Combee <petercombee@gmail.com>
2023-08-10 16:48:34 +0200

    Initial Commit

---
+++ FizzBuzz/FizzBuzz.h
@@ -0,0 +1,18 @@
+//
+//  FizzBuzz.h
+//  FizzBuzz
+//
+//  Created by Peter Combee on 10/08/2023.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for FizzBuzz.
+FOUNDATION_EXPORT double FizzBuzzVersionNumber;
+
+//! Project version string for FizzBuzz.
+FOUNDATION_EXPORT const unsigned char FizzBuzzVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FizzBuzz/PublicHeader.h>
+
+
