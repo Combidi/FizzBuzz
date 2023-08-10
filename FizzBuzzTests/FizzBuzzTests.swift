c166187679c68d82b48f8b150bb529c129aaddad
Peter Combee <petercombee@gmail.com>
2023-08-10 16:55:43 +0200

    `evaluate` delivers `Fizz` on numbers divisible by 3

--- FizzBuzzTests/FizzBuzzTests.swift
+++ FizzBuzzTests/FizzBuzzTests.swift
@@ -1,36 +1,29 @@
 //
-//  FizzBuzzTests.swift
-//  FizzBuzzTests
-//
 //  Created by Peter Combee on 10/08/2023.
 //
 
 import XCTest
-@testable import FizzBuzz
+
+// Fizz buzz
+// Delivers fizz on any number divisible by 3
+// Delivers buzz on any number divisible by 5
+// Delivers fissbuzz on any number that is divisible by both 3 and 5
+
+enum FizzBuzz {
+    static func evaluate(number: Int) -> String { "Fizz" }
+}
 
 final class FizzBuzzTests: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
+    func test_evaluate_deliversFizzOnNumbersDivisibleByThree() {
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+        let samples = [3, 6, 9, 12, 300]
+        
+        for sample in samples {
+            
+            let result = FizzBuzz.evaluate(number: sample)
+            
+            XCTAssertEqual(result, "Fizz")
         }
     }
-
 }
